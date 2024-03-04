3f6cd1acecd7e342f85d2b17484a879187ee6628
Ekaterina <katusham0912@gmail.com>
2024-03-04 16:04:33 +0700

    Реализация задачи "Движение фигур"
    Был рассмотрен код с методички. И исправление русификации

---
+++ MovingImages/mainwindow.cpp
@@ -0,0 +1,12 @@
+#include "mainwindow.h"
+
+Window::Window()
+{
+    setWindowTitle("Обработка событий");
+    area = new Area( this );
+    btn = new QPushButton("Завершить",this );
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    layout->addWidget(area);
+    layout->addWidget(btn);
+    connect(btn, SIGNAL(clicked(bool)),this,SLOT(close()));
+};
