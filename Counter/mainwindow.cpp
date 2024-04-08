0cfb20f34b13f558d7452215298c20b6b8ba5558
Ekaterina <katusham0912@gmail.com>
2024-04-08 13:02:29 +0700

    Обновлены сигнально-слотовые соединения

--- Counter/mainwindow.cpp
+++ Counter/mainwindow.cpp
@@ -3,13 +3,17 @@
 Win::Win(QWidget *parent):QWidget(parent)
 {
     setWindowTitle("Счетчик");
+    // Создаем метки для отображения текста и указываем родительский виджет (this)
     label1 = new QLabel("Cчет по 1",this);
     label2 = new QLabel("Cчет по 5",this);
+    // Создаем объекты класса Counter для редактирования текста и указываем начальный текст и родительский виджет (this)
     edit1 = new Counter("0",this);
     edit2 = new Counter("0",this);
+    // Создаем кнопки с текстом и указываем родительский виджет (this)
     calcbutton = new QPushButton("+1",this);
-    exitbutton=new QPushButton("Выход",this);
-    QHBoxLayout *layout1 = new QHBoxLayout();
+    exitbutton = new QPushButton("Выход",this);
+
+    QHBoxLayout *layout1 = new QHBoxLayout();//горизонтальный компоновщик – управляет расположением виджетов в окне по горизонтали;
     layout1->addWidget(label1);
     layout1->addWidget(label2);
     QHBoxLayout *layout2 = new QHBoxLayout();
@@ -18,12 +22,18 @@ Win::Win(QWidget *parent):QWidget(parent)
     QHBoxLayout *layout3 = new QHBoxLayout();
     layout3->addWidget(calcbutton);
     layout3->addWidget(exitbutton);
-    QVBoxLayout *layout4 = new QVBoxLayout(this);
+    // Создаем вертикальный компоновщик для объединения всех горизонтальных компоновщиков
+    QVBoxLayout *layout4 = new QVBoxLayout(this);//вертикальный компоновщик – управляет расположением виджетов в окне по вертикали;
     layout4->addLayout(layout1);
     layout4->addLayout(layout2);
     layout4->addLayout(layout3);
-// связь сигнала нажатия кнопки и слота закрытия окна
-    connect(calcbutton,SIGNAL(clicked(bool)),edit1,SLOT(add_one()));
-    connect(edit1,SIGNAL(tick_signal()),edit2,SLOT(add_one()));
-    connect(exitbutton,SIGNAL(clicked(bool)),this,SLOT(close()));
+
+    edit1->setEnabled(false);
+    edit2->setEnabled(false);
+
+    //связь сигнала нажатия кнопки и слота закрытия окна
+    //connect(&Отправитель, &Class_1::mysignal, &Адресат, &Class_2::myslot);
+    connect(calcbutton,&QPushButton::clicked,edit1,&Counter::add_one);//когда пользователь кликает на кнопку calcbutton, будет вызван слот add_one объекта edit1.
+    connect(edit1,&Counter::tick_signal,edit2,&Counter::add_one);//когда сигнал tick_signal будет отправлен объектом edit1, будет вызван слот add_one объекта edit2.
+    connect(exitbutton,&QPushButton::clicked,this,&Win::close);//Когда пользователь кликает на кнопку exitbutton, будет вызван слот close, что, вероятно, приведет к закрытию окна Win.
 }
--- Counter/mainwindow.h
+++ Counter/mainwindow.h
@@ -8,34 +8,36 @@
 #include <QPushButton>
 #include <QHBoxLayout>
 
-class Counter:public QLineEdit
+class Counter:public QLineEdit  //создаем новый класс редактируемой строки
 {
-    Q_OBJECT
+    Q_OBJECT  // макрос Qt, обеспечивающий корректное создание сигналов и слотов
 public:
     Counter(const QString & contents, QWidget *parent=0):
         QLineEdit(contents,parent){}
+    //конструктор. contents - начальный текст,который мы зададим и parent-родитель
 signals:
-    void tick_signal();
+    void tick_signal();//объявляем сигнал
 public slots:
-    void add_one()
+    void add_one()//слот add_one() увеличивает число на единицу, проверяет, кратно ли оно 5, и если да, отправляет сигнал tick_signal().
     {
-        QString str=text();
-        int r=str.toInt();
-        if (r!=0 && r%5 ==0) emit tick_signal();
-        r++;
-        str.setNum(r);
-        setText(str);
+        QString str=text();// в str записываем текст из текстового поля
+        int r=str.toInt();//Преобразует текст str в целое число и сохраняет его в переменную r.
+        if (r!=0 && r%5 ==0) emit tick_signal();//если число кратно 5, то вызываем сигнал tick_signal
+        r++;//увеличиваем на единицу
+        str.setNum(r);//Преобразует число r обратно в строку и сохраняем его в переменную str.
+        setText(str);//Устанавливает новое значение текстового поля на основе изменённой строки str.
     }
 };
-class Win: public QWidget
+
+class Win: public QWidget //новый класс окна
 {
     Q_OBJECT
 protected:
-    QLabel *label1,*label2;
-    Counter *edit1,*edit2;
-    QPushButton *calcbutton;
-    QPushButton *exitbutton;
+    QLabel *label1,*label2;//метки для отображения текста
+    Counter *edit1,*edit2;//Указатели на объекты класса Counter для редактирования текста
+    QPushButton *calcbutton;//кнопка для выполнения операции
+    QPushButton *exitbutton;//кнопка выхода
 public:
-    Win(QWidget *parent = 0);
+    Win(QWidget *parent = 0);//конструктор
 };
 #endif
